211e394c76d043ac45c916094988fcd8b215664f
Nad1Far <danilforwork0@gmail.com>
2025-03-23 12:35:28 +0500

    Заданий подряд больше не будет!!!

--- Assets/Scripts/TaskController.cs
+++ Assets/Scripts/TaskController.cs
@@ -5,32 +5,48 @@ using UnityEngine.UI;
 public class TaskController : MonoBehaviour
 {
     public GameObject Tasks;
-    private int randomChild1;
-    private int randomRandomChild1;
-    private int CountChild1;
+
+    private int randomChild;
+    private int randomRandomChild;
+    private int CountChild;
+
+    private int[] lastTask = new int[2];
+
     private void Start()
     {
-        int CountChild = Tasks.transform.childCount;
-        CountChild1 = CountChild;
+        CountChild = Tasks.transform.childCount;
         GetTask();
+        TaskActive();
+        lastTaskSave();
     }
 
     public void ButtonClicked()
     {
-        Tasks.transform.GetChild(randomChild1).transform.GetChild(randomRandomChild1).gameObject.SetActive(false);
+        Tasks.transform.GetChild(randomChild).transform.GetChild(randomRandomChild).gameObject.SetActive(false);
+
         GetTask();
 
+        while (randomChild == lastTask[0] & randomRandomChild == lastTask[1])
+        {
+            GetTask();
+        }
+        TaskActive();
+        lastTaskSave();
     }
 
     void GetTask()
     {
-        int randomChild = Random.Range(0, CountChild1);
+        randomChild = Random.Range(0, CountChild);
         int CountChildOfChild = Tasks.transform.GetChild(randomChild).transform.childCount;
-        int randomRandomChild = Random.Range(0, CountChildOfChild);
-
+        randomRandomChild = Random.Range(0, CountChildOfChild);
+    }
+    void TaskActive()
+    {
         Tasks.transform.GetChild(randomChild).transform.GetChild(randomRandomChild).gameObject.SetActive(true);
-
-        randomChild1 = randomChild;
-        randomRandomChild1 = randomRandomChild;
+    }
+    void lastTaskSave()
+    {
+        lastTask[0] = randomChild;
+        lastTask[1] = randomRandomChild;
     }
 }
