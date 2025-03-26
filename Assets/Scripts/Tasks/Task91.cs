cc4a37d279ac3f6c44f6b08e62dc5ba9a493d57d
Nad1Far <danilforwork0@gmail.com>
2025-03-26 12:03:55 +0500

    9.1

---
+++ Assets/Scripts/Tasks/Task91.cs
@@ -0,0 +1,39 @@
+using TMPro;
+using UnityEngine;
+
+public class Task91 : MonoBehaviour
+{
+    public GameObject Chisl1;
+    public GameObject Znam1;
+    public GameObject Znam2;
+    public GameObject RightChislo;
+
+    private int Order;
+
+    private string[] VarChisl1 = new string[10] {"0", "0", "0", "0", "0", "0", "0", "0", "0", "0"};
+    private string[] VarZnam1 = new string[10] { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+
+
+    private string[] VarChisl2 = new string[10] { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+    private string[] VarZnam2 = new string[10] { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+
+
+    private string[] VarAnswer = new string[10] { "0", "0", "0", "0", "0", "0", "0", "0", "0", "0" };
+
+    void Task()
+    {
+        Order = Random.Range(0, 20);
+        Chisl1.transform.GetComponent<TMP_Text>().text = VarChisl1[Order];
+        Znam1.transform.GetComponent<TMP_Text>().text = VarZnam1[Order];
+        RightChislo.transform.GetComponent<TMP_Text>().text = VarChisl2[Order];
+        Znam2.transform.GetComponent<TMP_Text>().text = VarZnam2[Order];
+    }
+
+    void Close()
+    {
+        Chisl1.transform.GetComponent<TMP_Text>().text = "";
+        Znam1.transform.GetComponent<TMP_Text>().text = "";
+        RightChislo.transform.GetComponent<TMP_Text>().text = "";
+        Znam2.transform.GetComponent<TMP_Text>().text = "";
+    }
+}
