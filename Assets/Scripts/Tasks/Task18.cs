18804d29a1198c89fc61630cad2c063bc9546f93
Nad1Far <danilforwork0@gmail.com>
2025-03-30 22:45:26 +0500

    готово

---
+++ Assets/Scripts/Tasks/Task18.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Task18 : MonoBehaviour
+{
+    public string Chislo;
+
+    void Task()
+    {
+        TaskController.TaskAnswer = Chislo;
+    }
+
+    void Close()
+    {
+
+    }
+}
