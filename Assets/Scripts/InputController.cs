75a53a11fa33984a69ca0514540aa473b7e69e69
Nad1Far <danilforwork0@gmail.com>
2025-04-05 14:43:40 +0500

    клавиатура

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -1,11 +1,16 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class InputController : MonoBehaviour
 {
     [SerializeField] private string MyText;
     [SerializeField] private TMP_InputField InputAnswer;
 
+    void Start()
+    {
+        InputAnswer.keyboardType = TouchScreenKeyboardType.NumberPad;
+    }
 
     public void ButtonClicked()
     {
